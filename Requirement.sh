a13e09783339c31671e15554aa07097097a420ae
n3tsh4d3 <adriano.condro@gmail.com>
2022-05-18 15:33:47 +0200

    dipendenze root

--- Requirement.sh
+++ Requirement.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-echo "Install Nmap, Nikto and SmbMap\n";
-apt install -y pip git nmap nikto smbmap wapiti ldap-utils polenum smbclient wget ;
-sudo pip install python-libnmap XlsxWriter impacket ldap3 PyYAML>=5.1;
-sudo pip3 install schedule
-
 #download arachni
 echo "\nDownload Arachni \n";
 wget https://github.com/Arachni/arachni/releases/download/v1.6.1.1/arachni-1.6.1.1-0.6.1.1-linux-x86_64.tar.gz;
@@ -12,22 +7,5 @@ gzip -d arachni-1.6.1.1-0.6.1.1-linux-x86_64.tar.gz;
 tar -xf arachni-1.6.1.1-0.6.1.1-linux-x86_64.tar;
 mv arachni-1.6.1.1-0.6.1.1 arachni;
 rm arachni-1.6.1.1-0.6.1.1-linux-x86_64.tar;
-#download script collection NSE Nmap
-echo "\nDownload Script collelction for Nmap \n";
-git clone https://github.com/n3tSh4d3/Nmap_Script_Collection.git;
-echo "\n Remove old script on /usr/share/nmap/scripts\n";
-rm -R /usr/share/nmap/scripts/*;
-echo "\nCopy Script collection on /usr/share/nmap/script\n";
-cd Nmap_Script_Collection;
-cp -R * /usr/share/nmap/scripts/;
-cd ..; rm -R Nmap_Script_Collection;
-echo "\nUpdate nmap db (--updatedb)\n";
-nmap --script-updatedb;
-echo "\nUpdate vulnscan script";
-cd /usr/share/nmap/scripts/vulscan/;
-echo "\nRemove old csv list exploit for vulscan script\n";
-rm *.csv*;
-chmod +x update.sh;
-echo "\nUpdate csv list for vulscan script\n";
-./update.sh;
+
 cd ;
---
+++ Root_Requirement.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Install Nmap, Nikto and SmbMap\n";
+apt install -y pip git nmap nikto smbmap wapiti ldap-utils polenum smbclient wget ;
+pip install python-libnmap XlsxWriter impacket ldap3 PyYAML>=5.1;
+pip3 install schedule;
+
+#download script collection NSE Nmap
+echo "\nDownload Script collelction for Nmap \n";
+git clone https://github.com/n3tSh4d3/Nmap_Script_Collection.git;
+echo "\n Remove old script on /usr/share/nmap/scripts\n";
+rm -R /usr/share/nmap/scripts/*;
+echo "\nCopy Script collection on /usr/share/nmap/script\n";
+cd Nmap_Script_Collection;
+cp -R * /usr/share/nmap/scripts/;
+cd ..; rm -R Nmap_Script_Collection;
+echo "\nUpdate nmap db (--updatedb)\n";
+nmap --script-updatedb;
+echo "\nUpdate vulnscan script";
+cd /usr/share/nmap/scripts/vulscan/;
+echo "\nRemove old csv list exploit for vulscan script\n";
+rm *.csv*;
+chmod +x update.sh;
+echo "\nUpdate csv list for vulscan script\n";
+./update.sh;
