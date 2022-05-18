90e091a72328973c0173e614302c91bc9b11ed29
n3tsh4d3 <adriano.condro@gmail.com>
2022-05-18 14:17:44 +0200

    aggiornato con download e installazione arachni

--- Requirement.sh
+++ Requirement.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
 echo "Install Nmap, Nikto and SmbMap\n";
-apt install -y pip git nmap nikto smbmap wapiti ldap-utils polenum smbclient ;
+apt install -y pip git nmap nikto smbmap wapiti ldap-utils polenum smbclient wget ;
 sudo pip install python-libnmap XlsxWriter impacket ldap3 PyYAML>=5.1;
 sudo pip3 install schedule
+
+#download arachni
+echo "\nDownload Arachni \n";
+wget https://github.com/Arachni/arachni/releases/download/v1.6.1.1/arachni-1.6.1.1-0.6.1.1-linux-x86_64.tar.gz;
+gzip -d arachni-1.6.1.1-0.6.1.1-linux-x86_64.tar.gz;
+tar -xf arachni-1.6.1.1-0.6.1.1-linux-x86_64.tar;
+mv arachni-1.6.1.1-0.6.1.1 arachni;
+rm arachni-1.6.1.1-0.6.1.1-linux-x86_64.tar;
+#download script collection NSE Nmap
 echo "\nDownload Script collelction for Nmap \n";
 git clone https://github.com/n3tSh4d3/Nmap_Script_Collection.git;
 echo "\n Remove old script on /usr/share/nmap/scripts\n";
