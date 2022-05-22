503427282599005247cf8e4342366b0fab868545
n3tsh4d3 <adriano.condro@gmail.com>
2022-05-22 11:44:07 +0200

    extract user for use on ncrack user list

--- NetShade_Network_Scanner.py
+++ NetShade_Network_Scanner.py
@@ -215,7 +215,7 @@ def deepScan():
                 print(Fore.RED + "Start Bruteforce Discovery Username and Password--- please wait!" + Style.RESET_ALL)
                 cmd = subprocess.run(
                     ['ncrack', "-vvv", "-m", "telnet:to=10", "-m", "mysql:ssl=yes,to=20", "-iX",
-                     repDir + "/Deep_Scan_Nmap_" + var_1 + ".xml", "-g", "to=30", "-f", "-U",
+                     repDir + "/Deep_Scan_Nmap_" + var_1 + ".xml", "-g", "to=60", "-f", "-U",
                      "dictionary/user_small.txt", "-P", "dictionary/pass_small.txt", "-oN",
                      repDir + "/Ncrack_password_discovered_" + var_1 + ".txt"])
             except KeyboardInterrupt:
@@ -440,13 +440,26 @@ def enumeration(a):
 
                     if bruteforce_SMB == 'Y':
                         try:
-                            cmd = subprocess.run(
-                                ["./enum4linux/enum4linux.py", "-U", ipScan, "|", "grep", "username", "|", "awk",
-                                 "'{print $2}'>" + repDir + "User_SMB_" + ipScan + ".txt"])
-                            cmd = subprocess.run(['ncrack', "-vv", ipScan, "-p", "smb", "-g", "to=30", "-f", "-U",
-                                                  repDir + "User_SMB_" + ipScan + ".txt", "-P",
-                                                  "dictionary/pass_small.txt", "-oN",
-                                                  repDir + "/Ncrack_SMB_pass_discovered_" + ipScan + ".txt"])
+                            s1 = open("Buffer" + repDir + "user_SMB.txt", "a")
+                            cmd = subprocess.run(["./enum4linux/enum4linux.py", "-U", ipScan], stdout=s1)
+                            s1.close()
+
+                            u = open("ListUser" + repDir + "user_SMB.txt", "a")
+                            cmd = subprocess.run(["grep", "username", "Buffer" + repDir + "user_SMB.txt"], stdout=u)
+                            u.close()
+
+                            k = open("User" + repDir + "user_SMB.txt", "a")
+                            cmd = subprocess.run(["awk", "{print $2}", "ListUser" + repDir + "user_SMB.txt"], stdout=k)
+                            k.close()
+
+                            with open("User" + repDir + "user_SMB.txt", "a") as f:
+                                f.write("guest\r\n")
+
+                            # cmd = subprocess.run(["./enum4linux/enum4linux.py", "-U", ipScan, "|", "grep", "username", "|", "awk","'{print $2}'>" + repDir + "User_SMB_" + ipScan + ".txt"])
+
+                            cmd = subprocess.run(['ncrack', "-vvv", ipScan, "-p", "smb", "-g", "to=30", "-f", "-U",
+                                              "User" + repDir + "user_SMB.txt", "-P", "dictionary/pass_small.txt",
+                                              "-oN", repDir + "/Ncrack_SMB_pass_discovered_" + ipScan + ".txt"])
                         except KeyboardInterrupt:
                             sys.exit()
 
